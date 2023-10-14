407d9d69963352389469dfbdf92d3cbbce5c9a05
gabriellecannella <73612182+gabriellecannella@users.noreply.github.com>
2023-10-13 20:30:48 -0400

    fixed footer and added new logo

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -1,6 +1,31 @@
 import React from "react";
 import "../../App.css";
+import "./AboutUs_page.css";
 
 export default function AboutUs() {
-  return <h1 className="about-us">ABOUT US</h1>;
+  return (
+    <>
+      <div className="about-us">
+        <h1>About Us</h1>
+      </div>
+      <div className="AboutUs_page">
+        <section className="about-section">
+          <h2>Our Story</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            euismod hendrerit urna, at feugiat velit vehicula ac.
+          </p>
+        </section>
+
+        <section className="about-section">
+          <h2>Our Team</h2>
+          <ul>
+            <li>John Doe - CEO</li>
+            <li>Jane Smith - COO</li>
+            <li>Alice Johnson - CTO</li>
+          </ul>
+        </section>
+      </div>
+    </>
+  );
 }
--- src/components/pages/Testimonials.js
+++ src/components/pages/Testimonials.js
@@ -1,6 +1,31 @@
 import React from "react";
+import "./Testimonials_page.css";
 import "../../App.css";
 
 export default function Testimonials() {
-  return <h1 className="testimonials">TESTIMONIALS</h1>;
+  return (
+    <>
+      <div className="testimonials">
+        <h1>Testimonial</h1>
+      </div>
+      <div className="testimonials_page">
+        <section className="about-section">
+          <h2>Our Story</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
+            euismod hendrerit urna, at feugiat velit vehicula ac.
+          </p>
+        </section>
+
+        <section className="about-section">
+          <h2>Our Team</h2>
+          <ul>
+            <li>John Doe - CEO</li>
+            <li>Jane Smith - COO</li>
+            <li>Alice Johnson - CTO</li>
+          </ul>
+        </section>
+      </div>
+    </>
+  );
 }
