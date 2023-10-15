59df74d20b64c488f35b961cfed3aba1a923b9c6
gabriellecannella <73612182+gabriellecannella@users.noreply.github.com>
2023-10-15 15:12:20 -0400

    goal counter styling

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,25 +9,17 @@ import Goal from "./Goal";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <AppProvider>
-        <div>
-          <h1>Student Attendance Goal</h1>
-          <Goal/>
-        </div>
-      </AppProvider>
-      {/* <AppProvider>
-        <div className='container'>
-          <h1 className='mt-3'>Student Attendance Goal</h1>
-          <div className='row mt-3'>
-            <div className='col-sm'>
-              <Goal/>
-            </div>
-          </div>
-        </div>
-      </AppProvider> */}
       <video src="/videos/home.mp4" autoPlay loop muted />
       <h1>UF IN GHANA</h1>
-      <p>Enroll in your Summer Internship Today </p>
+      <h2>
+        <AppProvider>
+          <div>
+            {" "}
+            <Goal />
+          </div>
+        </AppProvider>
+      </h2>
+      <p>SPOTS REMAINING </p>
       <div className="hero-btns">
         <Button_AboutUs
           className="btns"
