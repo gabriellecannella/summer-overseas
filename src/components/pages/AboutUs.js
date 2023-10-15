3f098c07a28583ae5a48f720d6f874385a296122
gabriellecannella <73612182+gabriellecannella@users.noreply.github.com>
2023-10-14 21:29:14 -0400

    About us page

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -10,20 +10,128 @@ export default function AboutUs() {
       </div>
       <div className="AboutUs_page">
         <section className="about-section">
-          <h2>Our Story</h2>
+          <h2>PROGRAM OVERVIEW</h2>
+          <h3>About Ghana</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
-            euismod hendrerit urna, at feugiat velit vehicula ac.
+            Ghana takes its name from the great medieval trading empire known as
+            the Ghana Empire. It was founded by the Akan Ethnic group in the 13
+            th century. The empire was rich in gold and traded widely with the
+            Portuguese, Dutch, English, and others between the 15 th and 16 th
+            centuries. This made it become known as the Gold Coast. The 17 th
+            century saw a shift into the slave trade, with devastating effects
+            on the country and huge implications for modern world history. Ghana
+            was the first African country south of the Sahara to gain
+            independence from the colonial rule of the British in 1957. Its
+            revolution was led by the nationalist and Pan-Africanist Kwame
+            Nkrumah, and the country has since become a model for African
+            economic recovery and political reform. Ghana is well known for
+            being among the world’s leading producers of high-quality sun-dried
+            cocoa. Agriculture, forestry, and fishing employ more than half of
+            the population. The country has a rich indigenous culture with
+            people groups such as the Ewe, Fante, Nzema, and Ga Dambe, all with
+            close ties to religion and institutions of chieftaincy. About 50
+            indigenous languages are spoken in Ghana, the major ones being Akan,
+            Ewe, Ga, Dagaare, and Dagbani. English is the official language.
+            Harvest season, marriage, birth, puberty, and death call for various
+            festivals and rites. These occasions are seen as an expression of
+            anti-colonialism in the 21 st century.{" "}
+          </p>
+          <p>
+            The focus of this curriculum is on internships and international
+            engineering concerns in Ghana and South Africa. Students will have
+            the chance to learn about the nature of computer science and global
+            engineering as well as acquire practical skills in computer science
+            innovation and social entrepreneurship. The program will integrate
+            Computer Science Principles and the Software Engineering Agile
+            Methodology to allow students to build innovative technologies to
+            address real world problems. Students will work for 6 weeks in Ghana
+            with local entrepreneurs in sectors including healthcare, education,
+            agriculture, and industry that heavily rely on artificial
+            intelligence. In order to determine the demands of the clients,
+            design a cutting-edge technological product, create a prototype, and
+            demonstrate the solution, the students will collaborate directly
+            with the clients. Students will gain proficiency in computer science
+            as well as communication to non-technical audiences.{" "}
+          </p>
+          <p>
+            Students will also learn about Ghana through historical and cultural
+            activities. They will learn to function and communicate successfully
+            in multicultural situations, which is critical for their success
+            throughout their internship and stay in Ghana. Students will return
+            with not only professional skills, but also a greater understanding
+            of another culture and an understanding of the function of
+            engineering in a global, economic, environmental, and cultural
+            context.
+          </p>
+          <div className="image-container">
+            <img src="images/img-15.jpeg" alt="Ghana Image 2" />
+          </div>
+        </section>
+
+        <section className="about-section">
+          <h2>TRANSFORMATIVE LEARNING JOURNEYS</h2>
+          <h3>Our Goals</h3>
+          <p>
+            EDU Africa aims to promote the process of student transformation by
+            utilizing the unique context of Africa to help students broaden
+            their intellectual horizons and grow personally, cross-culturally,
+            professionally, and as global citizens.
           </p>
         </section>
 
         <section className="about-section">
-          <h2>Our Team</h2>
-          <ul>
-            <li>John Doe - CEO</li>
-            <li>Jane Smith - COO</li>
-            <li>Alice Johnson - CTO</li>
-          </ul>
+          <h2>LEARNING OUTCOMES</h2>
+          <p>By the end of this program, students should be able to:</p>
+          <ol>
+            <li>
+              Identify, describe, and explain global and intercultural
+              conditions and interdependencies{" "}
+            </li>
+            <li>Analyze and interpret global and intercultural issues </li>
+            <li>Communicate effectively with members of other cultures </li>
+            <li>
+              Learn to embrace differences and foster intercultural technical
+              competency and communication
+            </li>
+            <li>Demonstrate increased personal self-awareness </li>
+            <li>
+              Gain an appreciation of the historical and cultural context of
+              Ghana{" "}
+            </li>
+            <li>
+              Demonstrate real world skills in computer science, innovation, and
+              entrepreneurship for social good{" "}
+            </li>
+            <li>
+              Recognize the competitive value of African innovation in the tech
+              entrepreneurship space globally and within Ghana{" "}
+            </li>
+          </ol>
+          <p></p>
+          <div className="image-container">
+            <img src="images/img-14.jpeg" alt="Ghana Image 2" />
+          </div>
+          <p></p>
+          <h3>Local program facilitator and or guide </h3>
+          <p>
+            All EDU Africa programs are implemented by trusted program
+            facilitators and/or guides. Our program facilitators and guides
+            undergo an intensive 12-module training series. The training modules
+            include details on EDU Africa’s transformation goals, reflective
+            practices, emergency procedures, child protection policies, and
+            communication skills, among other elements. We source and carefully
+            select our facilitators and guides from the region to ensure that
+            they can effectively meet specific program themes and needs.
+          </p>
+          <p>
+            Their embodiment of EDU Africa’s values of friendliness,
+            collaboration, innovation, stewardship, and learning helps them to
+            holistically support both leaders and participants throughout their
+            program. This guarantees that the planned itinerary and activities
+            come to life. The assigned program facilitator/s and/or guides will
+            be available at all times to assist with any student and educator
+            needs, thus ensuring the smooth running of the program.{" "}
+          </p>
         </section>
       </div>
     </>
